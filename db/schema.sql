89671b1406424759112f771ff28136918355c303
Barmaley <e.g.zakharov@gmail.com>
2022-04-26 19:59:17 +0300

    0. Spring DataSource [#6878 #292409]

--- db/schema.sql
+++ db/schema.sql
@@ -1,17 +1,27 @@
-CREATE TABLE accident (
-  id serial primary key,
-  name varchar(2000)
+create table if not exists accident (
+    id serial primary key,
+    name varchar(2000),
+    text varchar(2000),
+    address varchar(2000),
+    type_id integer references accident_type (id)
 );
 
-create table accident_type (
+create table if not exists accident_type (
     id serial primary key,
     name varchar(100)
 );
 
-create table accident_rule (
+create table if not exists accident_rule (
     id serial primary key,
     name varchar(100)
 );
+
+create table if not exists accidents_rules (
+    id serial primary key,
+    accident_id integer references accident (id),
+    rule_id  integer references accident_rule (id)
+);
+
 
 insert into accident_type (name) values
                                      ('Две машины'), ('Машина и человек'), ('Машина и велосипед');
--- src/main/java/accident/repositories/AccidentJdbcTemplate.java
+++ src/main/java/accident/repositories/AccidentJdbcTemplate.java
@@ -4,10 +4,12 @@ import accident.models.Accident;
 import accident.models.AccidentType;
 import accident.models.Rule;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
-import java.util.Collection;
-import java.util.List;
+import java.sql.PreparedStatement;
+import java.util.*;
 
 @Repository
 public class AccidentJdbcTemplate implements Store {
@@ -19,37 +21,63 @@ public class AccidentJdbcTemplate implements Store {
 
     @Override
     public Accident create(Accident accident, String[] ids) {
-        jdbc.update("insert into accident (name) values (?)",
-                accident.getName());
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        jdbc.update(connection -> {
+            PreparedStatement statement = connection.prepareStatement(
+                    "insert into accident(name, text, address, type_id) values(?, ?, ?, ?)",
+                    new String[]{"id"});
+            statement.setString(1, accident.getName());
+            statement.setString(2, accident.getText());
+            statement.setString(3, accident.getAddress());
+            statement.setInt(4, accident.getType().getId());
+            return statement;
+        }, keyHolder);
+        accident.setId((int) keyHolder.getKey());
+        for (String id : ids) {
+            jdbc.update("insert into accidents_rules (accident_id, rule_id) " +
+                            "values (?, ?) ",
+                    accident.getId(),
+                    Integer.parseInt(id));
+        }
         return accident;
     }
 
     @Override
     public void update(Accident accident) {
-        jdbc.update("update accident set name = ? where id = ?",
+        jdbc.update("update accident set name = ?, text = ?, address = ? where id = ?",
                 accident.getName(),
+                accident.getText(),
+                accident.getAddress(),
                 accident.getId()
         );
     }
 
     @Override
     public Accident get(int id) {
-        return jdbc.queryForObject("select id, name from accident where id = ?",
+        return jdbc.queryForObject("select * from accident where id = ?",
                 (rs, row) -> {
                     Accident accident = new Accident();
                     accident.setId(rs.getInt("id"));
                     accident.setName(rs.getString("name"));
+                    accident.setText(rs.getString("text"));
+                    accident.setAddress(rs.getString("address"));
+                    accident.setType(getAccidentType(rs.getInt("type_id")));
+                    accident.setRules(new HashSet<>(setRulesForAccident(accident)));
                     return accident;
                 }, id);
     }
 
     @Override
     public List<Accident> getAll() {
-        return jdbc.query("select id, name from accident order by id",
+        return jdbc.query("select * from accident order by id",
                 (rs, row) -> {
                     Accident accident = new Accident();
                     accident.setId(rs.getInt("id"));
                     accident.setName(rs.getString("name"));
+                    accident.setText(rs.getString("text"));
+                    accident.setAddress(rs.getString("address"));
+                    accident.setType(getAccidentType(rs.getInt("type_id")));
+                    accident.setRules(new HashSet<>(setRulesForAccident(accident)));
                     return accident;
                 });
     }
@@ -74,5 +102,29 @@ public class AccidentJdbcTemplate implements Store {
                     rule.setName(rs.getString("name"));
                     return rule;
                 });
+    }
+
+    private AccidentType getAccidentType(int id) {
+        return jdbc.queryForObject("select id, name from accident_type where id = ?",
+                (rs, row) -> {
+                    AccidentType accidentType = new AccidentType();
+                    accidentType.setId(rs.getInt("id"));
+                    accidentType.setName(rs.getString("name"));
+                    return accidentType;
+                }, id);
+    }
+
+    private Collection<Rule> setRulesForAccident(Accident accident) {
+        return jdbc.query(
+                "select rules.id, rules.name " +
+                        "from accidents_rules as ar " +
+                        "inner join accident_rule as rules " +
+                        "on ar.rule_id = rules.id and accident_id = ?;",
+                (rs, row) -> {
+                    Rule rule = new Rule();
+                    rule.setId(rs.getInt("id"));
+                    rule.setName(rs.getString("name"));
+                    return rule;
+                }, accident.getId());
     }
 }
